c7d837bf286394f21c2dc8c6b682e42a53d64fb2
scooby78 <mschuba@internode.on.net>
2017-10-13 13:30:30 +1000

    Removed spaces

--- SOR_Vietnam/cfgArmour.hpp
+++ SOR_Vietnam/cfgArmour.hpp
@@ -33,7 +33,7 @@ class SOR_MOG_open_Nam: rhsgref_nat_ural_open
 	displayName="M35 Military Truck Open";
 	editorCategory = "SOR_Cat_Faction_Nam";
 	faction = "SOR_Faction_Nam";
-	crew="SOR_Rifleman_Nam  ";
+	crew="SOR_Rifleman_Nam";
 	typicalCargo[]=	{"SOR_Rifleman_Nam"};
 	hiddenSelections[] = {"camo1","camo2","numplate","numplate2","n1","n2","n3","n4","i1","i2","i3","i4"};
 	hiddenSelectionsTextures[] = {"SOR_Vietnam\data\SOR_MOG_NAM.paa","rhsafrf\addons\rhs_a2port_car\ural\data\ural_plachta_co.paa"};
@@ -49,7 +49,7 @@ class SOR_FORDGP_open_Nam: rhsgref_nat_uaz_open
 	displayName="FORD GP Willys Jeep";
 	editorCategory = "SOR_Cat_Faction_Nam";
 	faction = "SOR_Faction_Nam";
-	crew="SOR_Rifleman_Nam  ";
+	crew="SOR_Rifleman_Nam";
 	typicalCargo[]=	{"SOR_Rifleman_Nam"};
 	hiddenSelections[] = {"camo1","camo2g","n1","n2","n3","n4","i1","i2","i3","i4"};
 	hiddenSelectionsTextures[] = {"SOR_Vietnam\data\SOR_Nam_Jeep.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa","rhsafrf\addons\RHS_Decals\Data\Labels\Misc\no_ca.paa"};
