b9a1f53355f5b35c6193cae5e13ef3583ceca53e
scooby78 <mschuba@internode.on.net>
2017-10-13 13:32:35 +1000

    Existing classnames removed

--- SOR_Vietnam/cfgEditorCategories.hpp
+++ SOR_Vietnam/cfgEditorCategories.hpp
@@ -9,87 +9,8 @@
 /////////////////////
 class CfgEditorCategories
 {
-	class SOR_Cat_Faction_W
-	{
-		displayName = "160th SOR (Woodland)";
-	};
 	class SOR_Cat_Faction_Nam
 	{
 		displayName = "160th SOR (Nam)";
 	};
-
-};
-/////////////////////////
-// 3DEN Sub Catagories //
-/////////////////////////
-class CfgEditorSubcategories
-{
-	class SOR_SubCat_SupplyBoxes 
-	{
-		displayName = "Boxes"; 
-	};
-	class SOR_SubCat_TrainingCrates 
-	{
-		displayName = "Crates Training"; 
-	};	
-	class SOR_SubCat_SupplyCrates
-	{
-		displayName = "Crates";
-	};
-	class SOR_SubCat_SupplyContainers
-	{
-		displayName = "Containers";
-	};
-	class SOR_SubCat_Lights
-	{
-		displayName = "Lights";
-	};
-	class SOR_SubCat_Pads
-	{
-		displayName = "Helipads";
-	};
-	class SOR_SubCat_Objects
-	{
-		displayName = "Objects";	
-	}
-	class SOR_SubCat_Radar
-	{
-		displayName = "Radar items";
-	};
-	class SOR_SubCat_Infantry
-	{
-		displayName = "Men (Infantry)";
-	};	
-	class SOR_SubCat_Infantry_AIR
-	{
-		displayName = "Men (Airborne)";
-	};	
-	class SOR_SubCat_Infantry_MECH
-	{
-		displayName = "Men (Mechanised)";
-	};		
-	class SOR_SubCat_Infantry_Support
-	{
-		displayName = "Men (Support)";
-	};	
-	class SOR_SubCat_Infantry_Recon
-	{
-		displayName = "Men (Recon)";
-	};	
-	class SOR_SubCat_Vehicles
-	{
-		displayName = "Armour";
-	};		
-	class SOR_SubCat_Aircraft
-	{
-		displayName = "Aircraft";
-	};
-	class SOR_SubCat_Aircraft_Captured
-	{
-		displayName = "Captured Aircraft";
-	};	
-	class SOR_SubCat_Vehicles_Truck
-	{
-		displayName = "Support Vehicles";
-	};		
 };
